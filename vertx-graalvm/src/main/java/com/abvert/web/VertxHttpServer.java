8f9489fd11b09e326bf3287aea02d32706d42678
clyhs <clygd@126.com>
2022-02-06 15:46:18 +0800

    bug

--- vertx-graalvm/src/main/java/com/abvert/web/VertxHttpServer.java
+++ vertx-graalvm/src/main/java/com/abvert/web/VertxHttpServer.java
@@ -3,17 +3,17 @@
  */
 package com.abvert.web;
 
-import java.io.File;
 
+import java.util.concurrent.TimeUnit;
 
 import com.abvert.web.verticle.MainVerticle;
 
-import io.vertx.core.logging.Logger;
-import io.vertx.core.logging.LoggerFactory;
-import io.vertx.core.logging.SLF4JLogDelegateFactory;
 import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Vertx;
 import io.vertx.core.VertxOptions;
+import io.vertx.core.http.HttpServer;
+import io.vertx.core.http.HttpServerOptions;
+import io.vertx.ext.web.Router;
 
 /**
  * @date Feb 5, 2022 8:24:41 PM
@@ -32,15 +32,37 @@ public class VertxHttpServer {
     	
         VertxOptions vertxOptions = new VertxOptions();
         Vertx vertx = Vertx.vertx(vertxOptions);
-
+        
         //部署http服务器
         vertx.deployVerticle(MainVerticle.class.getName(),
                 new DeploymentOptions().setInstances(VertxOptions.DEFAULT_EVENT_LOOP_POOL_SIZE), res -> {
             if(res.succeeded()){
                 //logger.warn("服务端部署成功----");
+            	System.out.println("服务端部署成功----");
             }else {
                 //logger.error("服务端部署失败---" + res.cause());
+            	System.out.println("服务端部署失败---" + res.cause());
             }
         });
+        /*
+        HttpServerOptions options = new HttpServerOptions().setIdleTimeout(10000)
+				.setIdleTimeoutUnit(TimeUnit.MILLISECONDS).setTcpKeepAlive(true).setSsl(false);
+
+        HttpServer httpServer = vertx.createHttpServer(options);
+
+		Router router = Router.router(vertx);
+		
+		router.route("/hello").handler(rc -> {
+			//log.info("Got hello request");
+			rc.response().end("World");
+		});
+		
+		httpServer.requestHandler(router).listen(8080, asyncResult -> {
+			if (asyncResult.failed()) {
+				System.out.println("start server failed! " + asyncResult.cause());
+			} else {
+				System.out.println("start server success!");
+			}
+		});*/
     }
 }
--- vertx-graalvm/src/main/java/com/abvert/web/substitutions/NettySubstitutions.java
+++ vertx-graalvm/src/main/java/com/abvert/web/substitutions/NettySubstitutions.java
@@ -15,132 +15,134 @@ import java.net.Inet6Address;
 import java.net.InetAddress;
 import java.util.function.Predicate;
 
-@TargetClass(className = "io.netty.buffer.AbstractReferenceCountedByteBuf", onlyWith = PlatformHasClass.class)
-final class Target_io_netty_buffer_AbstractReferenceCountedByteBuf {
-    @Alias
-    @RecomputeFieldValue(kind = Kind.FieldOffset, //
-            declClassName = "io.netty.buffer.AbstractReferenceCountedByteBuf", //
-            name = "refCnt") //
-    private static long REFCNT_FIELD_OFFSET;
-}
-
-@TargetClass(className = "io.netty.util.AbstractReferenceCounted", onlyWith = PlatformHasClass.class)
-final class Target_io_netty_util_AbstractReferenceCounted {
-    @Alias
-    @RecomputeFieldValue(kind = Kind.FieldOffset, //
-            declClassName = "io.netty.util.AbstractReferenceCounted", //
-            name = "refCnt") //
-    private static long REFCNT_FIELD_OFFSET;
-}
-
-@TargetClass(NetUtil.class)
-final class NetUtilSubstitutions {
-    @Alias
-    @InjectAccessors(NetUtilLocalhost4Accessor.class)
-    public static Inet4Address LOCALHOST4;
-
-    @Alias
-    @InjectAccessors(NetUtilLocalhost6Accessor.class)
-    public static Inet6Address LOCALHOST6;
-
-    private static class NetUtilLocalhost4Accessor {
-        static Inet4Address get() {
-            // using https://en.wikipedia.org/wiki/Initialization-on-demand_holder_idiom
-            return NetUtilLocalhost4LazyHolder.LOCALHOST4;
-        }
-
-        static void set(Inet4Address ignored) {
-            // a no-op setter to avoid exceptions when NetUtil is initialized at run-time
-        }
-    }
-
-    private static class NetUtilLocalhost4LazyHolder {
-        private static final Inet4Address LOCALHOST4;
-
-        static {
-            byte[] LOCALHOST4_BYTES = {127, 0, 0, 1};
-            // Create IPv4 loopback address.
-            try {
-                LOCALHOST4 = (Inet4Address) InetAddress.getByAddress("localhost", LOCALHOST4_BYTES);
-            } catch (Exception e) {
-                // We should not get here as long as the length of the address is correct.
-                PlatformDependent.throwException(e);
-                throw new IllegalStateException("Should not reach here");
-            }
-        }
-    }
-
-    private static class NetUtilLocalhost6Accessor {
-        static Inet6Address get() {
-            // using https://en.wikipedia.org/wiki/Initialization-on-demand_holder_idiom
-            return NetUtilLocalhost6LazyHolder.LOCALHOST6;
-        }
-
-        static void set(Inet6Address ignored) {
-            // a no-op setter to avoid exceptions when NetUtil is initialized at run-time
-        }
-    }
-    
-    private static class NetUtilLocalhost6LazyHolder {
-        private static final Inet6Address LOCALHOST6;
-
-        static {
-            byte[] LOCALHOST6_BYTES = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1};
-            // Create IPv6 loopback address.
-            try {
-                LOCALHOST6 = (Inet6Address) InetAddress.getByAddress("localhost", LOCALHOST6_BYTES);
-            } catch (Exception e) {
-                // We should not get here as long as the length of the address is correct.
-                PlatformDependent.throwException(e);
-                throw new IllegalStateException("Should not reach here");
-            }
-        }
-    }
-    
-    @Alias
-    @InjectAccessors(NetUtilLocalhostAccessor.class)
-    public static InetAddress LOCALHOST;
-
-    // NOTE: this is the simpliest implementation I could invent to just demonstrate the idea; it is probably not
-    // too efficient. An efficient implementation would only have getter and it would compute the InetAddress
-    // there; but the post is already very long, and NetUtil.LOCALHOST computation logic in Netty is rather cumbersome.
-    private static class NetUtilLocalhostAccessor {
-        private static volatile InetAddress ADDR;
-
-        static InetAddress get() {
-            return ADDR;
-        }
-
-        static void set(InetAddress addr) {
-            ADDR = addr;
-        }
-    }
-}
 
 
-@TargetClass(InternetProtocolFamily.class)
-final class InternetProtocolFamilySubstitutions {
-    @Alias
-    @InjectAccessors(InternetProtocolFamilyLocalhostAccessor.class)
-    private InetAddress localHost;
+//@TargetClass(className = "io.netty.buffer.AbstractReferenceCountedByteBuf", onlyWith = PlatformHasClass.class)
+//final class Target_io_netty_buffer_AbstractReferenceCountedByteBuf {
+//    @Alias
+//    @RecomputeFieldValue(kind = Kind.FieldOffset, //
+//            declClassName = "io.netty.buffer.AbstractReferenceCountedByteBuf", //
+//            name = "refCnt") //
+//    private static long REFCNT_FIELD_OFFSET;
+//}
+//
+//@TargetClass(className = "io.netty.util.AbstractReferenceCounted", onlyWith = PlatformHasClass.class)
+//final class Target_io_netty_util_AbstractReferenceCounted {
+//    @Alias
+//    @RecomputeFieldValue(kind = Kind.FieldOffset, //
+//            declClassName = "io.netty.util.AbstractReferenceCounted", //
+//            name = "refCnt") //
+//    private static long REFCNT_FIELD_OFFSET;
+//}
 
-    private static class InternetProtocolFamilyLocalhostAccessor {
-        static InetAddress get(InternetProtocolFamily family) {
-            switch (family) {
-                case IPv4:
-                    return NetUtil.LOCALHOST4;
-                case IPv6:
-                    return NetUtil.LOCALHOST6;
-                default:
-                    throw new IllegalStateException("Unsupported internet protocol family: " + family);
-            }
-        }
+//@TargetClass(NetUtil.class)
+//final class NetUtilSubstitutions {
+//    @Alias
+//    @InjectAccessors(NetUtilLocalhost4Accessor.class)
+//    public static Inet4Address LOCALHOST4;
+//
+//    @Alias
+//    @InjectAccessors(NetUtilLocalhost6Accessor.class)
+//    public static Inet6Address LOCALHOST6;
+//
+//    private static class NetUtilLocalhost4Accessor {
+//        static Inet4Address get() {
+//            // using https://en.wikipedia.org/wiki/Initialization-on-demand_holder_idiom
+//            return NetUtilLocalhost4LazyHolder.LOCALHOST4;
+//        }
+//
+//        static void set(Inet4Address ignored) {
+//            // a no-op setter to avoid exceptions when NetUtil is initialized at run-time
+//        }
+//    }
+//
+//    private static class NetUtilLocalhost4LazyHolder {
+//        private static final Inet4Address LOCALHOST4;
+//
+//        static {
+//            byte[] LOCALHOST4_BYTES = {127, 0, 0, 1};
+//            // Create IPv4 loopback address.
+//            try {
+//                LOCALHOST4 = (Inet4Address) InetAddress.getByAddress("localhost", LOCALHOST4_BYTES);
+//            } catch (Exception e) {
+//                // We should not get here as long as the length of the address is correct.
+//                PlatformDependent.throwException(e);
+//                throw new IllegalStateException("Should not reach here");
+//            }
+//        }
+//    }
+//
+//    private static class NetUtilLocalhost6Accessor {
+//        static Inet6Address get() {
+//            // using https://en.wikipedia.org/wiki/Initialization-on-demand_holder_idiom
+//            return NetUtilLocalhost6LazyHolder.LOCALHOST6;
+//        }
+//
+//        static void set(Inet6Address ignored) {
+//            // a no-op setter to avoid exceptions when NetUtil is initialized at run-time
+//        }
+//    }
+//    
+//    private static class NetUtilLocalhost6LazyHolder {
+//        private static final Inet6Address LOCALHOST6;
+//
+//        static {
+//            byte[] LOCALHOST6_BYTES = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1};
+//            // Create IPv6 loopback address.
+//            try {
+//                LOCALHOST6 = (Inet6Address) InetAddress.getByAddress("localhost", LOCALHOST6_BYTES);
+//            } catch (Exception e) {
+//                // We should not get here as long as the length of the address is correct.
+//                PlatformDependent.throwException(e);
+//                throw new IllegalStateException("Should not reach here");
+//            }
+//        }
+//    }
+//    
+//    @Alias
+//    @InjectAccessors(NetUtilLocalhostAccessor.class)
+//    public static InetAddress LOCALHOST;
+//
+//    // NOTE: this is the simpliest implementation I could invent to just demonstrate the idea; it is probably not
+//    // too efficient. An efficient implementation would only have getter and it would compute the InetAddress
+//    // there; but the post is already very long, and NetUtil.LOCALHOST computation logic in Netty is rather cumbersome.
+//    private static class NetUtilLocalhostAccessor {
+//        private static volatile InetAddress ADDR;
+//
+//        static InetAddress get() {
+//            return ADDR;
+//        }
+//
+//        static void set(InetAddress addr) {
+//            ADDR = addr;
+//        }
+//    }
+//}
 
-        static void set(InternetProtocolFamily family, InetAddress address) {
-            // storing nothing as the getter derives all it needs from its argument
-        }
-    }
-}
+
+//@TargetClass(InternetProtocolFamily.class)
+//final class InternetProtocolFamilySubstitutions {
+//    @Alias
+//    @InjectAccessors(InternetProtocolFamilyLocalhostAccessor.class)
+//    private InetAddress localHost;
+//
+//    private static class InternetProtocolFamilyLocalhostAccessor {
+//        static InetAddress get(InternetProtocolFamily family) {
+//            switch (family) {
+//                case IPv4:
+//                    return NetUtil.LOCALHOST4;
+//                case IPv6:
+//                    return NetUtil.LOCALHOST6;
+//                default:
+//                    throw new IllegalStateException("Unsupported internet protocol family: " + family);
+//            }
+//        }
+//
+//        static void set(InternetProtocolFamily family, InetAddress address) {
+//            // storing nothing as the getter derives all it needs from its argument
+//        }
+//    }
+//}
 
 /**
  * A predicate to tell whether this platform includes the argument class.
